fe4c0bdb3afb774756df734140e8bdf290448cff
CyberGhost <dcwebsols+github@gmail.com>
2025-12-07 19:58:00 +0530

    replit 0000

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('about-us');
+    
+    const navbar = document.getElementsByTagName('nav')[0];
+    if (navbar) {
+      navbar.classList.add('navbar-transparent');
+    }
   }
-
+  
+  ngOnDestroy(): void {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('about-us');
+    
+    const navbar = document.getElementsByTagName('nav')[0];
+    if (navbar) {
+      navbar.classList.remove('navbar-transparent');
+    }
+  }
 }
